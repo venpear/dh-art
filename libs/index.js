cbbbf44f6bca3b84aaea055f9216d8ed17229738
王敬博 <470665207@qq.com>
2018-03-21 16:57:07 +0800

    更新版本

--- libs/index.js
+++
@@ -1,7 +0,0 @@
-
-
-export { default as Tabs } from './Tabs';
-export { default as Collapse } from './Collapse';
-export { default as Modal } from './Modal';
-export { default as Input } from './Input';
-export { default as Select } from './Select';
